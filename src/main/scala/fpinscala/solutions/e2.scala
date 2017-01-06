8d10cf435a4127b9c79304a7161f2594cd903c96
Dan Brooks <dmbrooks@live.co.uk>
2017-01-06 12:56:33 +0000

    Tidy writing of annotations

--- src/main/scala/fpinscala/solutions/e2.scala
+++ src/main/scala/fpinscala/solutions/e2.scala
@@ -1,16 +1,16 @@
 package fpinscala.solutions
 
+import annotation.tailrec
+
 object e2 {
 
-  @annotation.tailrec
-  def fib(n: Int, prev: Int = 0, acc: Int = 1): Int = n match {
+  @tailrec def fib(n: Int, prev: Int = 0, acc: Int = 1): Int = n match {
     case 0 => n
     case 1 => acc
     case _ => fib(n - 1, acc, acc + prev)
   }
 
-  @annotation.tailrec
-  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+  @tailrec def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
     if (as.length < 2) true
     else ordered(as.head, as.tail.head) && isSorted(as.tail, ordered)
   }
