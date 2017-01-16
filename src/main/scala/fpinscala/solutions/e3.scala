453c0f0a68a806b5b858117d07cfee9e4071e925
Dan Brooks <dmbrooks@live.co.uk>
2017-01-16 22:38:43 +0000

    Refactor e3 solutions, separate list & tree

--- src/main/scala/fpinscala/solutions/e3.scala
+++ src/main/scala/fpinscala/solutions/e3.scala
@@ -5,131 +5,135 @@ import util.e3._
 
 object e3 {
 
-  import List._
+  object list {
+    import List._
 
-  def x(ls: List[Int]): Int = ls match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
-  }
+    def x(ls: List[Int]): Int = ls match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + sum(t)
+      case _ => 101
+    }
 
-  def tail[A](ls: List[A]): List[A] = ls match {
-    case Nil => sys.error("Cannot apply to empty list")
-    case Cons(_, tl) => tl
-  }
+    def tail[A](ls: List[A]): List[A] = ls match {
+      case Nil => sys.error("Cannot apply to empty list")
+      case Cons(_, tl) => tl
+    }
 
-  def setHead[A](hd: A, ls: List[A]): List[A] = ls match {
-    case Nil => sys.error("setCannot apply to empty list")
-    case Cons(_, tl) => Cons(hd, tl)
-  }
+    def setHead[A](hd: A, ls: List[A]): List[A] = ls match {
+      case Nil => sys.error("setCannot apply to empty list")
+      case Cons(_, tl) => Cons(hd, tl)
+    }
 
-  def drop[A](ls: List[A], n: Int): List[A] =
-    if (n <= 0) ls
-    else ls match {
+    def drop[A](ls: List[A], n: Int): List[A] =
+      if (n <= 0) ls
+      else ls match {
+        case Nil => Nil
+        case Cons(_, tl) => drop(tl, n - 1)
+      }
+
+    def dropWhile[A](ls: List[A], f: A => Boolean): List[A] = ls match {
       case Nil => Nil
-      case Cons(_, tl) => drop(tl, n - 1)
+      case Cons(hd, tl) => if (f(hd)) dropWhile(tl, f) else ls
     }
 
-  def dropWhile[A](ls: List[A], f: A => Boolean): List[A] = ls match {
-    case Nil => Nil
-    case Cons(hd, tl) => if (f(hd)) dropWhile(tl, f) else ls
-  }
+    def init[A](ls: List[A]): List[A] = ls match {
+      case Nil => sys.error("Cannot apply to empty list")
+      case Cons(_, Nil) => Nil
+      case Cons(hd, tl) => Cons(hd, init(tl))
+    }
 
-  def init[A](ls: List[A]): List[A] = ls match {
-    case Nil => sys.error("Cannot apply to empty list")
-    case Cons(_, Nil) => Nil
-    case Cons(hd, tl) => Cons(hd, init(tl))
-  }
+    def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(reverse(as), z)((b, a) => f(a, b))
+    }
 
-  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
-    case Nil => z
-    case Cons(x, xs) => foldLeft(reverse(as), z)((b, a) => f(a, b))
-  }
+    @tailrec def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
 
-  @tailrec def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match {
-    case Nil => z
-    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
-  }
+    def sum(ns: List[Int]): Int =
+      foldRight(ns, 0)(_ + _)
 
-  def sum(ns: List[Int]): Int =
-    foldRight(ns, 0)(_ + _)
+    def product(ns: List[Int]): Double =
+      foldRight(ns, 1.0)(_ * _)
 
-  def product(ns: List[Int]): Double =
-    foldRight(ns, 1.0)(_ * _)
+    def count[A](as: List[A]): Int =
+      foldRight(as, 0)((_, n) => n + 1)
 
-  def count[A](as: List[A]): Int =
-    foldRight(as, 0)((_, n) => n + 1)
+    def sumLeft(ns: List[Int]): Int =
+      foldLeft(ns, 0)(_ + _)
 
-  def sumLeft(ns: List[Int]): Int =
-    foldLeft(ns, 0)(_ + _)
+    def productLeft(ns: List[Int]): Double =
+      foldLeft(ns, 1.0)(_ * _)
 
-  def productLeft(ns: List[Int]): Double =
-    foldLeft(ns, 1.0)(_ * _)
+    def countLeft(ns: List[Int]): Int =
+      foldLeft(ns, 0)((n, _) => n + 1)
 
-  def countLeft(ns: List[Int]): Int =
-    foldLeft(ns, 0)((n, _) => n + 1)
+    def reverse[A](ns: List[A]): List[A] =
+      foldLeft(ns, Nil: List[A])((xs, x) => Cons(x, xs))
 
-  def reverse[A](ns: List[A]): List[A] =
-    foldLeft(ns, Nil: List[A])((xs, x) => Cons(x, xs))
+    def append[A](a1: List[A], a2: List[A]): List[A] =
+      foldRight(a1, a2)(Cons(_, _))
 
-  def append[A](a1: List[A], a2: List[A]): List[A] =
-    foldRight(a1, a2)(Cons(_, _))
+    def flatten[A](lists: List[List[A]]): List[A] =
+      foldRight(lists, List[A]())(append)
 
-  def flatten[A](lists: List[List[A]]): List[A] =
-    foldRight(lists, List[A]())(append)
+    def increment(numbers: List[Int]): List[Int] =
+      map(numbers)(_ + 1)
 
-  def increment(numbers: List[Int]): List[Int] =
-    map(numbers)(_ + 1)
+    def doublesToStrings(dubs: List[Double]): List[String] =
+      map(dubs)(_.toString)
 
-  def doublesToStrings(dubs: List[Double]): List[String] =
-    map(dubs)(_.toString)
+    def map[A, B](as: List[A])(f: A => B): List[B] =
+      foldRight(as, List[B]())((x, xs) => Cons(f(x), xs))
 
-  def map[A, B](as: List[A])(f: A => B): List[B] =
-    foldRight(as, List[B]())((x, xs) => Cons(f(x), xs))
+    def filter[A](as: List[A])(f: A => Boolean): List[A] =
+      flatMap(as)(a => if (f(a)) List(a) else Nil)
 
-  def filter[A](as: List[A])(f: A => Boolean): List[A] =
-    flatMap(as)(a => if (f(a)) List(a) else Nil)
+    def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
+      flatten(map(as)(f))
 
-  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
-    flatten(map(as)(f))
+    def combine(as: List[Int], bs: List[Int]): List[Int] =
+      zipWith(as, bs)((a: Int, b: Int) => a + b)
 
-  def combine(as: List[Int], bs: List[Int]): List[Int] =
-    zipWith(as, bs)((a: Int, b: Int) => a + b)
+    def zipWith[A](a: List[A], b: List[A])(f: (A, A) => A): List[A] = (a, b) match {
+      case (Nil, as) => as
+      case (as, Nil) => as
+      case (Cons(x, xs), Cons(y, ys)) => Cons(f(x, y), zipWith(xs, ys)(f))
+    }
 
-  def zipWith[A](a: List[A], b: List[A])(f: (A, A) => A): List[A] = (a, b) match {
-    case (Nil, as) => as
-    case (as, Nil) => as
-    case (Cons(x, xs), Cons(y, ys)) => Cons(f(x, y), zipWith(xs, ys)(f))
-  }
+    @tailrec def startsWith[A](list: List[A], check: List[A]): Boolean = (list, check) match {
+      case (_, Nil) => true
+      case (Cons(x, xs), Cons(y, ys)) => (x == y) && startsWith(xs, ys)
+      case _ => false
+    }
 
-  @tailrec def startsWith[A](list: List[A], check: List[A]): Boolean = (list, check) match {
-    case (_, Nil) => true
-    case (Cons(x, xs), Cons(y, ys)) => (x == y) && startsWith(xs, ys)
-    case _ => false
-  }
-
-  @tailrec def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {
-    if (startsWith(sup, sub)) true
-    else sup match {
-      case Nil => false
-      case Cons(_, xs) => hasSubsequence(xs, sub)
+    @tailrec def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {
+      if (startsWith(sup, sub)) true
+      else sup match {
+        case Nil => false
+        case Cons(_, xs) => hasSubsequence(xs, sub)
+      }
     }
   }
 
-  def treesize[T](t: Tree[T]): Int = t match {
-    case Leaf(_) => 1
-    case Branch(left, right) => 1 + treesize(left) + treesize(right)
-  }
+  object tree {
+    def size[T](t: Tree[T]): Int = t match {
+      case Leaf(_) => 1
+      case Branch(left, right) => 1 + size(left) + size(right)
+    }
 
-  def maximum(t: Tree[Int]): Int = t match {
-    case Leaf(value) => value
-    case Branch(left, right) => maximum(left) max maximum(right)
-  }
+    def maximum(t: Tree[Int]): Int = t match {
+      case Leaf(value) => value
+      case Branch(left, right) => maximum(left) max maximum(right)
+    }
 
-  def depth[T](t: Tree[T]): Int = t match {
-    case Leaf(_) => 1
-    case Branch(left, right) => 1 + depth(left) max depth(right)
+    def depth[T](t: Tree[T]): Int = t match {
+      case Leaf(_) => 1
+      case Branch(left, right) => 1 + depth(left) max depth(right)
+    }
   }
 }
--- src/test/scala/fpinscala/solutions/e3Spec.scala
+++ src/test/scala/fpinscala/solutions/e3Spec.scala
@@ -6,236 +6,240 @@ import org.scalatest._
 
 class e3Spec extends FreeSpec with Matchers {
 
-  def numbers: List[Int] = List(1,2,3,4,5)
+  "list" - {
+    def numbers: List[Int] = List(1,2,3,4,5)
 
-  "x" - {
-    x(numbers) should be (3)
-  }
+    "x" - {
+      list.x(numbers) should be (3)
+    }
 
-  "take" - {
-    "should return the list without the first element of the list" in {
-      tail(numbers) should be (List(2, 3, 4, 5))
+    "take" - {
+      "should return the list without the first element of the list" in {
+        list.tail(numbers) should be (List(2, 3, 4, 5))
+      }
+    }
+
+    "setHead" - {
+      "should replace the first element of the list" in {
+        list.setHead(7, numbers) should be (List(7, 2, 3, 4, 5))
+      }
+    }
+
+    "drop" - {
+      "should drop the provided n number of elements" in {
+        list.drop(numbers, 2) should be (List(3, 4, 5))
+        list.drop(numbers, 5) should be (Nil)
+        list.drop(numbers, 3) should be (List(4, 5))
+      }
+    }
+
+    "dropWhile" - {
+      "should drop elements while the predicate matches true" in {
+        list.dropWhile(numbers, (n: Int) => n < 3) should be (List(3, 4, 5))
+        list.dropWhile(numbers, (n: Int) => n > 0) should be (Nil)
+        list.dropWhile(numbers, (n: Int) => n < 4) should be (List(4, 5))
+      }
+    }
+
+    "init" - {
+      "should drop the last element in the list" in {
+        list.init(numbers) should be (List(1, 2, 3, 4))
+        list.init(list.init(numbers)) should be (List(1, 2, 3))
+      }
+    }
+
+    "foldRight" - {
+      "should fold right" in {
+        list.foldRight(numbers, 0)(_ - _) should be (3)
+      }
+    }
+
+    "foldLeft" - {
+      "should fold left" in {
+        list.foldLeft(numbers, 0)(_ - _) should be (-15)
+      }
+    }
+
+    "sum" - {
+      "should sum all the numbers" in {
+        list.sum(numbers) should be (15)
+      }
+    }
+
+    "product" - {
+      "should get the product of all the numbers" in {
+        list.product(numbers) should be (120)
+      }
+    }
+
+    "count" - {
+      "should return the number of elements in the list" in {
+        list.count(numbers) should be (5)
+      }
+    }
+
+    "foldLeft" - {
+      "should implement sum" in {
+        list.sumLeft(numbers) should be (15)
+      }
+
+      "should implement product" in {
+        list.productLeft(numbers) should be (120)
+      }
+
+      "should implement count" in {
+        list.countLeft(numbers) should be (5)
+      }
+    }
+
+    "reverse" - {
+      "should reverse the list" in {
+        list.reverse(numbers) should be (List(5, 4, 3, 2, 1))
+      }
+    }
+
+    "append" - {
+      "should append a list to another list" in {
+        list.append(numbers, List(6, 7, 8)) should be (List(1, 2, 3, 4, 5, 6, 7, 8))
+      }
+    }
+
+    "flatten" - {
+      "should flatten a list of lists" in {
+        list.flatten(List(numbers, numbers, List(4, 3, 2, 1))) should be (List(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 4, 3, 2, 1))
+      }
+    }
+
+    "increment" - {
+      "should increment the numbers" in {
+        list.increment(numbers) should be (List(2, 3, 4, 5, 6))
+      }
+    }
+
+    "doublesToStrings" - {
+      "should convert a list of doubles to a list of strings" in {
+        list.doublesToStrings(List(1.3, 3.4)) should be (List("1.3", "3.4"))
+      }
+    }
+
+    "map" - {
+      "should map over items in list in place returning transformed list" in {
+        list.map(numbers)(_ * 2) should be (List(2, 4, 6, 8, 10))
+      }
+    }
+
+    "filter" - {
+      "should filter items from a list that do not match predicate" in {
+        list.filter(numbers)(_ % 2 == 0) should be (List(2, 4))
+      }
+    }
+
+    "flatMap" - {
+      "should map over items in list, returning new list but merging into flattened list" in {
+        list.flatMap(List(1, 2, 3))(i => List(i, i)) should be (List(1, 1, 2, 2, 3, 3))
+      }
+    }
+
+    "combine" - {
+      "should combine items from two lists to make a single list" in {
+        list.combine(List(1, 2, 3), List(4, 5, 6)) should be (List(5, 7, 9))
+        list.combine(List(1, 2, 3), numbers) should be (List(2, 4, 6, 4, 5))
+      }
+    }
+
+    "zipWith" - {
+      "should combine using provided function" in {
+        val firstNames = List("Martin", "Paul", "Runar")
+        val lastNames = List("Odersky", "Chiusano", "Bjarnason")
+
+        list.zipWith(firstNames, lastNames)((first, last) => s"$first $last") should be (List("Martin Odersky", "Paul Chiusano", "Runar Bjarnason"))
+      }
+    }
+
+    "startsWith" - {
+      "should return true for list [1,2,3,4] starting with [1]" in {
+        list.startsWith(List(1,2,3,4), List(1)) should be (true)
+      }
+
+      "should return true for list [1,2,3,4] starting with [1,2]" in {
+        list.startsWith(List(1,2,3,4), List(1,2)) should be (true)
+      }
+
+      "should return true for list [1,2,3,4] starting with [1,2,3]" in {
+        list.startsWith(List(1,2,3,4), List(1,2,3)) should be (true)
+      }
+
+      "should return true for list [1,2,3,4] starting with [1,2,3,4]" in {
+        list.startsWith(List(1,2,3,4), List(1,2,3,4)) should be (true)
+      }
+
+      "should return false for list [1,2,3,4] starting with [1,2,3,4,5]" in {
+        list.startsWith(List(1,2,3,4), List(1,2,3,4,5)) should be (false)
+      }
+
+      "should return false for list [1,2,3,4] starting with [2,3,4]" in {
+        list.startsWith(List(1,2,3,4), List(2,3,4)) should be (false)
+      }
+    }
+
+    "hasSubsequence" - {
+      "should return true for list [1, 2, 3, 4] containing subsequence [1, 2]" in {
+        list.hasSubsequence(List(1, 2, 3, 4), List(1, 2)) should be (true)
+      }
+
+      "should return true for list [1, 2, 3, 4] containing subsequence [2, 3]" in {
+        list.hasSubsequence(List(1, 2, 3, 4), List(2, 3)) should be (true)
+      }
+
+      "should return true for list [1, 2, 3, 4] containing subsequence [4]" in {
+        list.hasSubsequence(List(1, 2, 3, 4), List(4)) should be (true)
+      }
+
+      "should return false for list [1, 2, 3, 4] containing subsequence [3, 1]" in {
+        list.hasSubsequence(List(1, 2, 3, 4), List(3, 1)) should be (false)
+      }
+
+      "should return false for list [1, 2, 3, 4] containing subsequence [5]" in {
+        list.hasSubsequence(List(1, 2, 3, 4), List(5)) should be (false)
+      }
+
+      "should return false for list [1, 2, 3, 4] containing subsequence [6, 4]" in {
+        list.hasSubsequence(List(1, 2, 3, 4), List(6, 4)) should be (false)
+      }
     }
   }
 
-  "setHead" - {
-    "should replace the first element of the list" in {
-      setHead(7, numbers) should be (List(7, 2, 3, 4, 5))
-    }
-  }
+  "tree" - {
+    "size" - {
+      "should return 1 for single leaf" in {
+        tree.size(Leaf(1)) should be (1)
+      }
 
-  "drop" - {
-    "should drop the provided n number of elements" in {
-      drop(numbers, 2) should be (List(3, 4, 5))
-      drop(numbers, 5) should be (Nil)
-      drop(numbers, 3) should be (List(4, 5))
-    }
-  }
+      "should return 3 for branch with two leaves" in {
+        tree.size(Branch(Leaf(1), Leaf(2))) should be (3)
+      }
 
-  "dropWhile" - {
-    "should drop elements while the predicate matches true" in {
-      dropWhile(numbers, (n: Int) => n < 3) should be (List(3, 4, 5))
-      dropWhile(numbers, (n: Int) => n > 0) should be (Nil)
-      dropWhile(numbers, (n: Int) => n < 4) should be (List(4, 5))
-    }
-  }
-
-  "init" - {
-    "should drop the last element in the list" in {
-      init(numbers) should be (List(1, 2, 3, 4))
-      init(init(numbers)) should be (List(1, 2, 3))
-    }
-  }
-
-  "foldRight" - {
-    "should fold right" in {
-      foldRight(numbers, 0)(_ - _) should be (3)
-    }
-  }
-
-  "foldLeft" - {
-    "should fold left" in {
-      foldLeft(numbers, 0)(_ - _) should be (-15)
-    }
-  }
-
-  "sum" - {
-    "should sum all the numbers" in {
-      sum(numbers) should be (15)
-    }
-  }
-
-  "product" - {
-    "should get the product of all the numbers" in {
-      product(numbers) should be (120)
-    }
-  }
-
-  "count" - {
-    "should return the number of elements in the list" in {
-      count(numbers) should be (5)
-    }
-  }
-
-  "foldLeft" - {
-    "should implement sum" in {
-      sumLeft(numbers) should be (15)
+      "should return 5 for a branch with one nested branch and one right leaf" in {
+        tree.size(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) should be (5)
+      }
     }
 
-    "should implement product" in {
-      productLeft(numbers) should be (120)
+    "maximum" - {
+      "should return the maximum element in tree" in {
+        tree.maximum(Leaf(1)) should be (1)
+        tree.maximum(Branch(Leaf(1), Leaf(2))) should be (2)
+        tree.maximum(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) should be (3)
+        tree.maximum(Branch(Branch(Leaf(7), Leaf(2)), Leaf(3))) should be (7)
+      }
     }
 
-    "should implement count" in {
-      countLeft(numbers) should be (5)
-    }
-  }
-
-  "reverse" - {
-    "should reverse the list" in {
-      reverse(numbers) should be (List(5, 4, 3, 2, 1))
-    }
-  }
-
-  "append" - {
-    "should append a list to another list" in {
-      append(numbers, List(6, 7, 8)) should be (List(1, 2, 3, 4, 5, 6, 7, 8))
-    }
-  }
-
-  "flatten" - {
-    "should flatten a list of lists" in {
-      flatten(List(numbers, numbers, List(4, 3, 2, 1))) should be (List(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 4, 3, 2, 1))
-    }
-  }
-
-  "increment" - {
-    "should increment the numbers" in {
-      increment(numbers) should be (List(2, 3, 4, 5, 6))
-    }
-  }
-
-  "doublesToStrings" - {
-    "should convert a list of doubles to a list of strings" in {
-      doublesToStrings(List(1.3, 3.4)) should be (List("1.3", "3.4"))
-    }
-  }
-
-  "map" - {
-    "should map over items in list in place returning transformed list" in {
-      map(numbers)(_ * 2) should be (List(2, 4, 6, 8, 10))
-    }
-  }
-
-  "filter" - {
-    "should filter items from a list that do not match predicate" in {
-      filter(numbers)(_ % 2 == 0) should be (List(2, 4))
-    }
-  }
-
-  "flatMap" - {
-    "should map over items in list, returning new list but merging into flattened list" in {
-      flatMap(List(1, 2, 3))(i => List(i, i)) should be (List(1, 1, 2, 2, 3, 3))
-    }
-  }
-
-  "combine" - {
-    "should combine items from two lists to make a single list" in {
-      combine(List(1, 2, 3), List(4, 5, 6)) should be (List(5, 7, 9))
-      combine(List(1, 2, 3), numbers) should be (List(2, 4, 6, 4, 5))
-    }
-  }
-
-  "zipWith" - {
-    "should combine using provided function" in {
-      val firstNames = List("Martin", "Paul", "Runar")
-      val lastNames = List("Odersky", "Chiusano", "Bjarnason")
-
-      zipWith(firstNames, lastNames)((first, last) => s"$first $last") should be (List("Martin Odersky", "Paul Chiusano", "Runar Bjarnason"))
-    }
-  }
-
-  "startsWith" - {
-    "should return true for list [1,2,3,4] starting with [1]" in {
-      startsWith(List(1,2,3,4), List(1)) should be (true)
-    }
-
-    "should return true for list [1,2,3,4] starting with [1,2]" in {
-      startsWith(List(1,2,3,4), List(1,2)) should be (true)
-    }
-
-    "should return true for list [1,2,3,4] starting with [1,2,3]" in {
-      startsWith(List(1,2,3,4), List(1,2,3)) should be (true)
-    }
-
-    "should return true for list [1,2,3,4] starting with [1,2,3,4]" in {
-      startsWith(List(1,2,3,4), List(1,2,3,4)) should be (true)
-    }
-
-    "should return false for list [1,2,3,4] starting with [1,2,3,4,5]" in {
-      startsWith(List(1,2,3,4), List(1,2,3,4,5)) should be (false)
-    }
-
-    "should return false for list [1,2,3,4] starting with [2,3,4]" in {
-      startsWith(List(1,2,3,4), List(2,3,4)) should be (false)
-    }
-  }
-
-  "hasSubsequence" - {
-    "should return true for list [1, 2, 3, 4] containing subsequence [1, 2]" in {
-      hasSubsequence(List(1, 2, 3, 4), List(1, 2)) should be (true)
-    }
-
-    "should return true for list [1, 2, 3, 4] containing subsequence [2, 3]" in {
-      hasSubsequence(List(1, 2, 3, 4), List(2, 3)) should be (true)
-    }
-
-    "should return true for list [1, 2, 3, 4] containing subsequence [4]" in {
-      hasSubsequence(List(1, 2, 3, 4), List(4)) should be (true)
-    }
-
-    "should return false for list [1, 2, 3, 4] containing subsequence [3, 1]" in {
-      hasSubsequence(List(1, 2, 3, 4), List(3, 1)) should be (false)
-    }
-
-    "should return false for list [1, 2, 3, 4] containing subsequence [5]" in {
-      hasSubsequence(List(1, 2, 3, 4), List(5)) should be (false)
-    }
-
-    "should return false for list [1, 2, 3, 4] containing subsequence [6, 4]" in {
-      hasSubsequence(List(1, 2, 3, 4), List(6, 4)) should be (false)
-    }
-  }
-
-  "size" - {
-    "should return 1 for single leaf" in {
-      treesize(Leaf(1)) should be (1)
-    }
-
-    "should return 3 for branch with two leaves" in {
-      treesize(Branch(Leaf(1), Leaf(2))) should be (3)
-    }
-
-    "should return 5 for a branch with one nested branch and one right leaf" in {
-      treesize(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) should be (5)
-    }
-  }
-
-  "maximum" - {
-    "should return the maximum element in tree" in {
-      maximum(Leaf(1)) should be (1)
-      maximum(Branch(Leaf(1), Leaf(2))) should be (2)
-      maximum(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) should be (3)
-      maximum(Branch(Branch(Leaf(7), Leaf(2)), Leaf(3))) should be (7)
-    }
-  }
-
-  "depth" - {
-    "should return the depth of a tree" in {
-      depth(Leaf(1)) should be (1)
-      depth(Branch(Leaf(1), Leaf(2))) should be (2)
-      depth(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) should be (3)
-      depth(Branch(Branch(Branch(Branch(Leaf(7), Leaf(2)), Leaf(2)), Leaf(2)), Leaf(3))) should be (5)
+    "depth" - {
+      "should return the depth of a tree" in {
+        tree.depth(Leaf(1)) should be (1)
+        tree.depth(Branch(Leaf(1), Leaf(2))) should be (2)
+        tree.depth(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) should be (3)
+        tree.depth(Branch(Branch(Branch(Branch(Leaf(7), Leaf(2)), Leaf(2)), Leaf(2)), Leaf(3))) should be (5)
+      }
     }
   }
 }
