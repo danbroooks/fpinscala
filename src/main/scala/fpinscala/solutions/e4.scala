cf1eb3a05071f9c10898ad82c5558517d7d22774
Dan Brooks <dmbrooks@live.co.uk>
2017-01-23 21:05:12 +0000

    Solve exercise 4.2, implement variance function

--- src/main/scala/fpinscala/solutions/e4.scala
+++ src/main/scala/fpinscala/solutions/e4.scala
@@ -1,5 +1,15 @@
 package fpinscala.solutions
 
-object e4 {
+import util.e4._
 
+object e4 {
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs).flatMap(m => {
+      mean(xs.map(x => math.pow(x - m, 2)))
+    })
+  }
+
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
 }
--- src/test/scala/fpinscala/solutions/e4Spec.scala
+++ src/test/scala/fpinscala/solutions/e4Spec.scala
@@ -53,4 +53,32 @@ class e4Spec extends FreeSpec with Matchers {
       }
     }
   }
+
+  "mean" - {
+    "should calculate the mean of a Seq(3, 3, 3)" in {
+      mean(Seq(3, 3, 3)) should be (Some(3.0))
+    }
+
+    "should calculate the mean of a Seq(5, 4, 3)" in {
+      mean(Seq(5, 4, 3)) should be (Some(4.0))
+    }
+
+    "should calculate the mean of a Seq(20, 18, 16, 14)" in {
+      mean(Seq(20, 18, 16, 14)) should be (Some(17.0))
+    }
+  }
+
+  "variance" - {
+    "should calculate the variance of a Seq(3, 3, 3)" in {
+      variance(Seq(3, 3, 3)) should be (Some(0))
+    }
+
+    "should calculate the variance of a Seq(5, 10, 5)" in {
+      variance(Seq(5, 10, 5)) should be (Some(5.5555555555555545))
+    }
+
+    "should calculate the variance of a Seq(20, 18, 16, 14)" in {
+      variance(Seq(20, 18, 16, 14)) should be (Some(5.0))
+    }
+  }
 }
