8e695d8c95e065908b70ca7b2216aebc75fe27e2
Dan Brooks <dmbrooks@live.co.uk>
2017-01-24 22:38:04 +0000

    Add Either type with method stubs for Exercise 4.6

---
+++ src/main/scala/fpinscala/solutions/util/e4/Either.scala
@@ -0,0 +1,20 @@
+package fpinscala.solutions.util.e4
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = ???
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+}
+
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Try {
+  def apply[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch { case e: Exception => Left(e) }
+}
