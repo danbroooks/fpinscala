fcefd87a3c24b1c3a32f3a1dbf0465230d06cd8d
Dan Brooks <dmbrooks@live.co.uk>
2017-05-24 17:54:26 +0100

    Complete exercise 6.2

--- src/main/scala/fpinscala/solutions/purely_functional_state/Exercises.scala
+++ src/main/scala/fpinscala/solutions/purely_functional_state/Exercises.scala
@@ -8,7 +8,7 @@ object Exercises {
    * Exercise 6.1
    *
    * Write a function that uses RNG.nextInt to generate a random integer between
-   * 0 and Int.maxValue (inclusive). Make sure to handle the corner case when
+   * 0 and Int.MaxValue (inclusive). Make sure to handle the corner case when
    * nextInt returns Int.MinValue, which doesn’t have a non-negative counterpart
    */
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
@@ -16,5 +16,15 @@ object Exercises {
     val shift = if (n < 0) 1 else 0
     (Math.abs(n) + shift, next)
   }
+
+  /**
+   * Exercise 6.2
+   *
+   * Write a function to generate a Double between 0 and 1, not including 1.
+   */
+   def double(rng: RNG): (Double, RNG) = {
+     val (n, next) = nonNegativeInt(rng)
+     (n / (Int.MaxValue.toDouble + 1), next)
+   }
 }
 
--- src/test/scala/fpinscala/solutions/purely_functional_state/ExercisesSpec.scala
+++ src/test/scala/fpinscala/solutions/purely_functional_state/ExercisesSpec.scala
@@ -7,7 +7,7 @@ import org.scalatest._
 
 class ExercisesSpec extends FreeSpec with Matchers {
   "nonNegativeInt" - {
-    "sould return the value of nextInt if the random number is positive" in {
+    "should return the value of nextInt if the random number is positive" in {
       val generator = SimpleRNG(90)
       nonNegativeInt(generator) should be(generator.nextInt)
     }
@@ -15,6 +15,13 @@ class ExercisesSpec extends FreeSpec with Matchers {
     "should keep generating random numbers until result is positive" in {
       val generator = SimpleRNG(-90)
       nonNegativeInt(generator) should be(34627403,SimpleRNG(279205635358137L))
+    }
+  }
+
+  "double" - {
+    "generates a double between 0 and 1, not including 1" in {
+      val generator = SimpleRNG(90)
+      double(generator) should be(0.016124640125781298,SimpleRNG(2269341352541L))
     }
   }
 }
